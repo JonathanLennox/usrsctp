339ae58cbf3a7c244cb9d23c336a54ace687e144
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-01-30 15:50:00 -0500

    Revert "Assert if soref() is called on an already-released socket."
    Unfortunately userspace sockets are sometimes created with early ref count 0.
    
    This reverts commit b4ba94ae4677f771e216693c96d92ef7ce13f5c3.

--- usrsctplib/user_socketvar.h
+++ usrsctplib/user_socketvar.h
@@ -403,7 +403,6 @@ void	sofree(struct socket *so);
 
 #define	soref(so) do {							\
 	SOCK_LOCK_ASSERT(so);						\
-	KASSERT((so)->so_count > 0, ("soref"));				\
 	++(so)->so_count;						\
 } while (0)
 
